1d3f3ab1ff3c9bdcd30edf6a8dfd85f8dcdc66cf
DrVrej <DrVrej@users.noreply.github.com>
2021-12-31 01:43:46 -0500

    2021 --> 2022

--- lua/entities/npc_vj_hlr2_zombine_slump/init.lua
+++ lua/entities/npc_vj_hlr2_zombine_slump/init.lua
@@ -1,14 +1,14 @@
 AddCSLuaFile("shared.lua")
 include('shared.lua')
 /*-----------------------------------------------
-	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
+	*** Copyright (c) 2012-2022 by DrVrej, All rights reserved. ***
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
 ENT.Slump = true
 ENT.Model = {"models/vj_hlr/hl2/zombine.mdl"}
 /*-----------------------------------------------
-	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
+	*** Copyright (c) 2012-2022 by DrVrej, All rights reserved. ***
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
