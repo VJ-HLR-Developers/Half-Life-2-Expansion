705169cd2403e4139e1ada817cb028ef5f20d768
Cpt-Hazama <Caleb123459@gmail.com>
2022-03-13 20:49:05 -0400

    Lamarr
    - Forgot to update Lamarr

--- lua/entities/npc_vj_hlr2_lamarr/init.lua
+++ lua/entities/npc_vj_hlr2_lamarr/init.lua
@@ -7,69 +7,58 @@ include('shared.lua')
 -----------------------------------------------*/
 ENT.Model = {"models/Lamarr.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
 ENT.StartHealth = 200
+
+ENT.VJ_NPC_Class = {"CLASS_PLAYER_ALLY"}
 ENT.FriendsWithAllPlayerAllies = true
 ENT.PlayerFriendly = true
-ENT.VJ_NPC_Class = {"CLASS_PLAYER_ALLY"} -- NPCs with the same class with be allied to each other
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnInitialize()
 	self:SetCollisionBounds(Vector(8,10,15), Vector(-8,-10,0))
-	self.NextSleepAnimT = CurTime()
-	self.NextSleepT = CurTime() +math.random(20,30)
-	self.WakeUpT = 0
-	self.IsSleeping = false
+
+	self.Headcrab_Sleeping = false
+	self.Headcrab_NextSleepT = CurTime() +math.random(20,30)
+	self.Headcrab_WakeUpT = 0
+	self.Headcrab_SleepAnimT = 0
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function ENT:CustomOnAlert(ent)
+	if self.Headcrab_Sleeping then
+		self.Headcrab_WakeUpT = 0
+	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnTakeDamage_OnBleed(dmginfo, hitgroup)
-	if self.IsSleeping then
-		self.WakeUpT = 0
+	if self.Headcrab_Sleeping then
+		self.Headcrab_WakeUpT = 0
 	else
-		self.NextSleepT = CurTime() +math.random(20,30)
+		self.Headcrab_NextSleepT = CurTime() +math.random(20,30)
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function ENT:CustomOnThink()
-	if !IsValid(self:GetEnemy()) then
-		if !self.IsSleeping then
-			if CurTime() > self.NextSleepT && math.random(1,30) == 1 then
-				self.WakeUpT = CurTime() +math.Rand(50,80)
-				self.IsSleeping = true
-			end
-		else
-			if CurTime() > self.WakeUpT then
-				self.IsSleeping = false
-				self.WakeUpT = 0
-				self.NextSleepT = CurTime() +math.random(20,30)
-			end
-		end
-	else
-		if self.IsSleeping then
-			self.IsSleeping = false
-			self:StartEngineTask(GetTaskList("TASK_RESET_ACTIVITY"),0)
-			self.WakeUpT = 0
-			self.NextSleepT = CurTime() +math.random(20,30)
-		end
-	end
-	if self.IsSleeping then
-		self:StopMoving()
-		self:StopMoving()
-		self:ClearSchedule()
-		self.HasSounds = false
-		self.SightDistance = 200
-		self.DisableWandering = true
-		self.DisableChasingEnemy = true
-		self.DisableMakingSelfEnemyToNPCs = true
-		self:SetMaxYawSpeed(0)
-		if CurTime() > self.NextSleepAnimT then
-			self:VJ_PlaySequence("SleepLoop",1,false,0,true)
+function ENT:CustomOnThink_AIEnabled()
+
+	if self.Headcrab_Sleeping then
+		if CurTime() > self.Headcrab_SleepAnimT then
+			self:VJ_ACT_PLAYACTIVITY("SleepLoop",true,false,false)
 			VJ_EmitSound(self,"npc/headcrab/idle2.wav",65,100)
-			self.NextSleepAnimT = CurTime() +self:SequenceDuration("SleepLoop")
+			self.Headcrab_SleepAnimT = CurTime() +self:SequenceDuration("SleepLoop")
+		elseif CurTime() > self.Headcrab_WakeUpT then
+			self.Headcrab_Sleeping = false
+			self.Headcrab_NextSleepT = CurTime() + math.Rand(15, 45)
+			self.SightDistance = 10000
+			self.SightAngle = 80
 		end
-	else
-		self.HasSounds = true
-		self.SightDistance = 10000
-		self.DisableWandering = false
-		self.DisableChasingEnemy = false
-		self.DisableMakingSelfEnemyToNPCs = false
-		self:SetMaxYawSpeed(self.TurningSpeed)
+		return
+	end
+
+	if self.VJ_IsBeingControlled then return end
+	
+	if !self.Alerted && !IsValid(self:GetEnemy()) && !self:IsMoving() && CurTime() > self.Headcrab_NextSleepT && !self.Headcrab_Sleeping && !self:IsBusy() then
+		local sleept = math.Rand(15, 30)
+		self.Headcrab_Sleeping = true
+		self.Headcrab_WakeUpT = CurTime() +math.Rand(15, 30)
+		self:SetState(VJ_STATE_ONLY_ANIMATION)
+		self.SightDistance = 325
+		self.SightAngle = 180
 	end
 end
