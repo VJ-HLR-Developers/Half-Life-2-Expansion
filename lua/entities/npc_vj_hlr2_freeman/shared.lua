4545ef0421c1b46cffd3fb021cf9683b3a89895d
DrVrej <DrVrej@users.noreply.github.com>
2022-07-12 02:35:19 -0400

    Updated to support VJ Tags

--- lua/entities/npc_vj_hlr2_freeman/shared.lua
+++ lua/entities/npc_vj_hlr2_freeman/shared.lua
@@ -5,6 +5,6 @@ ENT.Author 			= "Cpt. Hazama"
 ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
 ENT.Purpose 		= "Spawn it and fight with it!"
 ENT.Instructions 	= "Click on the spawnicon to spawn it."
-ENT.Category		= "Half-Life"
+ENT.Category		= "Half-Life Resurgence"
 
 ENT.VJ_HLR_Freeman = true
