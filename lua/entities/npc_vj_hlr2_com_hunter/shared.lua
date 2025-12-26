9cf1545f734362d4f82e721de5fda9de79866d51
KM_CM <send.nex@gmail.com>
2025-12-26 21:53:38 +0200

    Small optimization

--- lua/entities/npc_vj_hlr2_com_hunter/shared.lua
+++ lua/entities/npc_vj_hlr2_com_hunter/shared.lua
@@ -9,14 +9,16 @@ if CLIENT && GetConVar("vj_hlr2_combine_eyeglow"):GetInt() == 1 then
     local mat = Material("sprites/light_glow02_add")
     local size = 20
     local col = Color(131, 224, 255)
+	local render_SetMaterial = render.SetMaterial
+	local render_DrawSprite = render.DrawSprite
 	function ENT:Draw()
 		self:DrawModel()
-        render.SetMaterial(mat)
+        render_SetMaterial(mat)
         for i = 1, 2 do
             local att = self:GetAttachment(self:LookupAttachment(i == 2 && "bottom_eye" or "top_eye"))
             local glowOrigin = att.Pos +att.Ang:Forward() *-5
-            render.DrawSprite(glowOrigin, size, size, col)
-            render.DrawSprite(glowOrigin, size, size, col)
+            render_DrawSprite(glowOrigin, size, size, col)
+            render_DrawSprite(glowOrigin, size, size, col)
         end
     end
 end
